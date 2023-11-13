989502ae4a97268d1379a1e9a21e1b6717201b4e
laurenschrader <laurenschrader123@gmail.com>
2023-11-12 20:34:11 -0800

    Add intial working yes/no form question.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,14 @@
-function resultsGenerator() {
-  let resultsString = "";
-  
-
-}
+// new code below
+console.log("before")
+window.addEventListener("load", function() {
+  console.log("after load")
+  let form = document.querySelector("form");
+  console.log(form)
+  form.addEventListener("submit", function(event) {
+    console.log(document.querySelector("div#result"))
+    console.log("after")
+    document.querySelector("div.result").removeAttribute("class");
+    event.preventDefault();
+  }); 
+// new code below  
+});
