2262b93cb2e134a611df217dbe40f4719a39235e
laurenschrader <laurenschrader123@gmail.com>
2023-11-13 02:10:00 -0800

    Add framework question 2, with working next button.

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,54 @@
 console.log("before")
+
 window.addEventListener("load", function () {
   console.log("after load")
-  let form = document.querySelector("form");
-  console.log(form)
-  form.addEventListener("submit", function (event) {
-    console.log(document.querySelector("div#result"))
-    console.log("after")
-    document.querySelector("div.result").removeAttribute("class");
+
+  const form = document.querySelector("#formQuestions");
+  //next button - 1
+  // nextButton fxn as variable:
+  const nextButton = document.querySelectorAll(".nextButton");
+
+  //my function to handle "next" button clicks
+  function handleNextButtonClick(event) {
     event.preventDefault();
+    const currentQuestion = event.target.closest(".form-group");
+    const nextQuestionId = event.target.getAttribute("data-target");
+    const nextQuestion = document.querySelector(nextQuestionId);
+
+    //unhide next question:
+    if (nextQuestion) {
+      nextQuestion.classList.remove("hidden")
+    }
+  }
+
+  //event listeners to each next button clicks:
+  nextButton.forEach(function (button) {
+    button.addEventListener("click", handleNextButtonClick);
+  });
+
+  //event listener for final form submission
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+    console.log("form submitted");
+
+    let yesCounter = 0;
+
+    // check each question value:
+    const question1 = document.querySelector('input[name="question1"]:checked');
+    const question2 = document.querySelector('input[name="question2"]:checked');
+
+    //sum up total yes counts:
+    if (question1 && question1.value === 'yes') yesCounter++;
+    if (question2 && question2.value === 'yes') yesCounter++;
+
+    console.log(yesCounter);
+
+    if (yesCounter >= 2) {
+      document.querySelector(".result").textContent = "You should study Python.";
+    } else {
+      document.querySelector(".result").textContent = "You should study C++";
+    }
+
+    document.querySelector("div.result").removeAttribute("class");
   });
 });
