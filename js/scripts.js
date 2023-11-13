f871a322a51bdb685d2ab20b41a1b31f869f1163
laurenschrader <laurenschrader123@gmail.com>
2023-11-12 20:35:56 -0800

    Format code structure.

--- js/scripts.js
+++ js/scripts.js
@@ -1,14 +1,12 @@
-// new code below
 console.log("before")
-window.addEventListener("load", function() {
+window.addEventListener("load", function () {
   console.log("after load")
   let form = document.querySelector("form");
   console.log(form)
-  form.addEventListener("submit", function(event) {
+  form.addEventListener("submit", function (event) {
     console.log(document.querySelector("div#result"))
     console.log("after")
     document.querySelector("div.result").removeAttribute("class");
     event.preventDefault();
-  }); 
-// new code below  
+  });
 });
